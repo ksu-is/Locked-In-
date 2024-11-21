bb9c8d168645036779e0ddd3d9ca29a5d5706eef
e1iane <etchassa@students.kennesaw.edu>
2024-11-20 20:46:26 -0500

    to-do.py

---
+++ to-do.py
@@ -0,0 +1,57 @@
+tasks = []
+
+
+def addTask():
+  task = input("Enter Task Here!: ")
+  tasks.append(task)
+  print(f"Task '{task}' added to the list.")
+
+
+def listTasks():
+  if not tasks:
+    print("You currently have no tasks! <3")
+  else:
+    print("Current Tasks:")
+    for index, task in enumerate(tasks):
+      print(f"Task #{index}. {task}")
+
+
+def deleteTask():
+  listTasks()
+  try:
+    taskToDelete = int(input("Enter the # to delete: "))
+    if taskToDelete >= 0 and taskToDelete < len(tasks):
+      tasks.pop(taskToDelete)
+      print(f"Task {taskToDelete} has been removed.")
+    else:
+      print(f"Task #{taskToDelete} was not found.")
+  except:
+    print("Invalid input.")
+
+
+if __name__ == "__main__":
+  ### Create a loop to run the app
+  print("Welcome in! Let's see what we gotta do!:)")
+  while True:
+    print("\n")
+    print("Please select one of the following options")
+    print("------------------------------------------")
+    print("1. Add a new task")
+    print("2. Delete a task")
+    print("3. List tasks")
+    print("4. Quit")
+
+    choice = input("Enter your choice: ")
+
+    if (choice == "1"):
+      addTask()
+    elif (choice == "2"):
+      deleteTask()
+    elif (choice == "3"):
+      listTasks()
+    elif (choice == "4"):
+      break
+    else:
+      print("Invalid input. Let's Try That Again!")
+
+  print("See you later👋! Make sure you continue to stay LockedIn🔒")
